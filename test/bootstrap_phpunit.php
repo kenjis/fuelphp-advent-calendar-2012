1a8a06497cf8a4b6aa3d6f24b9907119858cfca2
kenjis <kenji.uui@gmail.com>
2012-12-02 20:24:51 +0900

    Composerの配置変更

--- test/bootstrap_phpunit.php
+++ test/bootstrap_phpunit.php
@@ -3,4 +3,5 @@
 define('TESTPATH', realpath('.') . '/');
 
 require TESTPATH . '../libs/BlogArticle.php';
+require TESTPATH . '../vendor/autoload.php';
 
--- test/phpunit.sh
+++ test/phpunit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-vendor/bin/phpunit $@ 
+../vendor/bin/phpunit $@
 
