525d12d26cd547a600494ef106bf4fa930384cdd
kenjis <kenji.uui@gmail.com>
2012-12-13 06:36:16 +0900

    画像の解像度変更スクリプト

---
+++ review/convert-image.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo "Convert PNG image density"
+  echo " usage: $0 (dir)"
+  echo "    eg: $0 images/01"
+  exit
+fi
+
+list=`find $1 -type f -name *.png`
+
+for file in $list; do
+  echo "Converting PNG density: $file"
+  convert "$file" "$file.bmp"
+  # I don't know why, but this converts png density to about 150 dpi
+  convert -density 59 "$file.bmp" "$file"
+  rm "$file.bmp"
+done
+
