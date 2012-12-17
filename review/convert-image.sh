0fb806dc642256141895d66f8a16d6e3b79840ed
kenjis <kenji.uui@gmail.com>
2012-12-17 09:49:50 +0900

    画像の解像度変更スクリプトの改良
    JPEG画像も処理するように

--- review/convert-image.sh
+++ review/convert-image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
-  echo "Convert PNG image density"
+  echo "Convert PNG/JPEG image density"
   echo " usage: $0 <dir>"
   echo "    eg: $0 images/01"
   exit
@@ -11,6 +11,16 @@ list=`find $1 -type f -name *.png`
 
 for file in $list; do
   echo "Converting PNG density: $file"
+  convert "$file" "$file.bmp"
+  # I don't know why, but this converts png density to about 150 dpi
+  convert -density 59 "$file.bmp" "$file"
+  rm "$file.bmp"
+done
+
+list=`find $1 -type f -name *.jpg`
+
+for file in $list; do
+  echo "Converting JPEG density: $file"
   convert "$file" "$file.bmp"
   # I don't know why, but this converts png density to about 150 dpi
   convert -density 59 "$file.bmp" "$file"
